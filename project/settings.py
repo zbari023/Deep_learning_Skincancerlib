de8f79b5f58168a9270fd450c683863d9ac7df28
zbari023 <barry450643@gmail.com>
2023-12-11 20:08:58 +0100

    add the static and media url

--- project/settings.py
+++ project/settings.py
@@ -115,7 +115,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'             # --------------
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+MEDIA_URL = '/media/'               # --------------
+MEDIA_ROOT = BASE_DIR / "media"     # --------------
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
