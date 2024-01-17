52832f143c80f26659b41166bac3f69ddd22d392
zbari023 <barry450643@gmail.com>
2024-01-17 22:07:35 +0100

    creating the first function in view.py and index.html in templates

--- project/urls.py
+++ project/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from skinlib.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home),
 ]
 
 
--- skinlib/views.py
+++ skinlib/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Image
 # Create your views here.
+
+
+def home(request):
+    data = Image.objects.all()
+    return render(request,'skinlib/index.html',{'data':data})
+
