81ff85ce25cb25fbc22a22d46f528c5bb5c1e74a
zbari023 <barry450643@gmail.com>
2024-01-18 20:23:02 +0100

    install tensorflow and change the setting dir to try the connection with CNN-Modell

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,11 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'             # --------------
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-MEDIA_URL = '/media/'               # --------------
-MEDIA_ROOT = BASE_DIR / "media"     # --------------
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- skinlib/views.py
+++ skinlib/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render, redirect
+from django.conf import settings
 from .models import Image
 from .forms import ImageUploadForm
+from django.core.files.storage import FileSystemStorage
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.applications.efficientnet import preprocess_input
+from tensorflow.keras.models import load_model
+import os
+import numpy as np
+from pathlib import Path
 # Create your views here.
+
+# Define constants
+BASE_DIR = Path(settings.BASE_DIR)
+MODEL_PATH = str(BASE_DIR / 'cnnmodell' / 'skin_cancer_model_multible_classifier.h5')
+CLASS_NAMES = [
+    'Actinic keratosis', 'Basal cell carcinoma', 'Benign keratosis', 'Dermatofibroma',
+    'Melanocytic nevus', 'Melanoma', 'Squamous cell carcinoma', 'Vascular lesion'
+]
+
+# Load the model outside of the view function
+CUSTOM_MODEL = load_model(MODEL_PATH)
+
+
 
 
 def home(request):
